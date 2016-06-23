db7373e34b46063edc9d1fc64d09b6ed14613ab2
Stefan Staub <ste.staub@gmail.com>
2016-06-23 23:42:16 +0200

    added inline documentation for percentize

--- lib/discountable/active_record/model.rb
+++ lib/discountable/active_record/model.rb
@@ -1,9 +1,37 @@
 module Discountable
-
   module ActiveRecord
-
     module Model
 
+      # Turns one or more columns into a Percentage type.
+      # = Options
+      # * <tt>postfix: ''</tt>appends the given postfix for the created attribute readers and writers, default is ''
+      # * <tt>validate: true</tt>Validation options for numericality validation. set to false if no validation
+      # should be done. Default is {greater_or_equal_than: 0}
+      #
+      # == Example Usage
+      #
+      #   class Order < ActiveRecord::Base
+      #     percentize :discount1, :discount2, postfix: 'percent'
+      #     percentize :tax, validate: { less_or_equal_than: 30 }
+      #   end
+      #
+      #   my_order = Order.new discount1: 10, discount2: 40, tax: 10
+      #   my_order.valid?
+      #   => # true
+      #   my_order.discount1
+      #   => # 10
+      #   my_order.discount1_percent
+      #   => # Percentage<10%>
+      #   my_order.tax
+      #   => # Percentage<10%>
+      #
+      #   my_order.tax = Percentage.new 50
+      #   my_order.valid?
+      #   => # false
+      #
+      # == Values outside percentage range
+      # You can set a value outside the allowed range for Percentage. The accessor then just returns the decimal
+      # value without conversion, so validation is possible.
       def percentize(*args)
 
         _default_options = { postfix: '', validate: {greater_or_equal_than: 0} }
